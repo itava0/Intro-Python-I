10ec791c5355001d254972e69623c78e00a17404
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-07-10 17:51:17 -0400

    Fourth Challenge Completed

--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,27 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+for argument in dir(sys.argv): print("Argument: ", argument)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print('PLATFORM :', sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print('IMPLEMENTATION VERSION INFORMATION', sys.implementation)
+print('VERSIONS INFORMATION: ', sys.version_info)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print('CURRENT PROCESS ID: ', os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print('CURRENT WORKING DIRECTORY (CWD): ', os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print('CURRENT LOCAL MACHINE LOGIN NAME: ', os.getlogin())
